412870593fdfd2616e98253ac1a3df16b4136604
Kanybek <kanybektlgn0@gmail.com>
2024-06-03 21:23:14 +0500

    feat(design-system): add button tabs

---
+++ design-system/ui/components/button-tabs/index.tsx
@@ -0,0 +1,31 @@
+import { Typography } from '@material-tailwind/react'
+import React, { FC, useState } from 'react'
+
+interface ButtonTabsProps {
+  className?: string
+  options: { label: string; value: string; Icon: React.ReactNode }[]
+}
+
+export const ButtonTabs: FC<ButtonTabsProps> = ({ className, options }) => {
+  const [active, setActive] = useState<string | null>(options[0]?.value || null)
+
+  const onChange = (value: string) => {
+    setActive(value)
+  }
+
+  return (
+    <div className="flex gap-4">
+      {options.map(({ label, value, Icon }) => (
+        <div
+          className={`flex gap-2 items-center rounded-lg transition-all duration-300 ${active === value ? 'bg-gray-100' : ''} cursor-pointer px-[8px] py-[6px] ${className}`}
+          onClick={() => onChange(value)}
+        >
+          {Icon}
+          <div>
+            <Typography className="text-sm leading-none font-normal text-gray-900">{label}</Typography>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
