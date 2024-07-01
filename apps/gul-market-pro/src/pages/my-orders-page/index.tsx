b1bd76b26f8b87ae29652d7acfc0f8771564c437
Kanybek <kanybektlgn0@gmail.com>
2024-07-01 19:48:13 +0500

    feat(pro): add rating modal

--- apps/gul-market-pro/src/pages/my-orders-page/index.tsx
+++ apps/gul-market-pro/src/pages/my-orders-page/index.tsx
@@ -3,6 +3,8 @@ import { Layout, OrderCard, OrderTabs, ScreenTemplate, useTabs } from '@design-s
 import { orders, tabs } from './constants'
 import { IOrders, ORDER_STATUS } from './types'
 import { formatOrderData } from './helpers'
+import { Dialog } from '@material-tailwind/react'
+import { RatingModal } from './rating-modal'
 
 export const MyOrdersPage = () => {
   const { activeTab, onTabChange } = useTabs<ORDER_STATUS>({ tabs })
@@ -14,7 +16,7 @@ export const MyOrdersPage = () => {
       {
         className: 'text-base font-medium px-[21px] py-[13px] bg-pink-500 normal-case min-w-[210px]',
         label: 'Отследить заказ',
-        handler: () => setDeliveryModalOpen(true)
+        handler: () => console.log('work')
       }
     ],
     [ORDER_STATUS.PLANTATION_DECLINED]: [
@@ -23,6 +25,7 @@ export const MyOrdersPage = () => {
         label: 'Оценить заказ',
         handler: (data: IOrders) => {
           console.log('Оценить заказ', data)
+          setDeliveryModalOpen(true)
         }
       },
       {
@@ -36,32 +39,39 @@ export const MyOrdersPage = () => {
   }
 
   return (
-    <Layout isLogged fullHeader>
-      <Layout.Content>
-        <ScreenTemplate title="Мои заказы">
-          <OrderTabs active={activeTab} tabs={tabs} onChange={onTabChange} />
-          <div className="flex flex-col gap-[12px] mt-[12px]">
-            {formatOrderData(
-              orders.filter(({ status }) => status === activeTab),
-              ACTION_HANDLERS
-            ).map(({ title, subTitle, imageUrl, logo, name, flag, price, priceDollar, priceTenge, actions }, index) => (
-              <OrderCard
-                key={index}
-                name={name}
-                title={title}
-                subTitle={subTitle}
-                imageUrl={imageUrl}
-                logo={logo}
-                flag={flag}
-                price={price}
-                priceDollar={priceDollar}
-                priceTenge={priceTenge}
-                actions={actions}
-              />
-            ))}
-          </div>
-        </ScreenTemplate>
-      </Layout.Content>
-    </Layout>
+    <>
+      <Layout isLogged fullHeader>
+        <Layout.Content>
+          <ScreenTemplate title="Мои заказы">
+            <OrderTabs active={activeTab} tabs={tabs} onChange={onTabChange} />
+            <div className="flex flex-col gap-[12px] mt-[12px]">
+              {formatOrderData(
+                orders.filter(({ status }) => status === activeTab),
+                ACTION_HANDLERS
+              ).map(
+                ({ title, subTitle, imageUrl, logo, name, flag, price, priceDollar, priceTenge, actions }, index) => (
+                  <OrderCard
+                    key={index}
+                    name={name}
+                    title={title}
+                    subTitle={subTitle}
+                    imageUrl={imageUrl}
+                    logo={logo}
+                    flag={flag}
+                    price={price}
+                    priceDollar={priceDollar}
+                    priceTenge={priceTenge}
+                    actions={actions}
+                  />
+                )
+              )}
+            </div>
+          </ScreenTemplate>
+        </Layout.Content>
+        <Dialog className="!min-w-fit !max-w-fit" open={deliveryModalOpen} handler={() => setDeliveryModalOpen(false)}>
+          <RatingModal onClose={() => setDeliveryModalOpen(false)} />
+        </Dialog>
+      </Layout>
+    </>
   )
 }
